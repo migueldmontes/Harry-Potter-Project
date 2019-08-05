7199cc01612bd8a906137c1d08f462e971257fd0
Miguel Angel Diaz Montes <48031654+migueldmontes@users.noreply.github.com>
2019-08-06 01:08:07 +0200

    Add files via upload

---
+++ proyectoFinal.js
@@ -0,0 +1,71 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+setInterval(() => {
+  var slides = document.getElementsByClassName("mySlides");
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  if (slideIndex < 1) {slideIndex = slides.length}  
+  showSlides(slideIndex);
+  slideIndex++;
+}, 4000)
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none"; 
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1} 
+//   slides[slideIndex-1].style.display = "block"; 
+//   setTimeout(showSlides, 5000); // Change image every 5 seconds
+// }
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// List View
+function listView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "100%";
+  }
+}
+
+// Grid View
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "50%";
+  }
+}
